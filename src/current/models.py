04d1920ba02dd53fc5334a17760ed12f0b68f06c
Maks <am.xlexeev@gmail.com>
2024-01-23 11:45:21 +0300

    add Rates model

--- src/current/models.py
+++ src/current/models.py
@@ -1,3 +1,29 @@
+from datetime import datetime
+
 from django.db import models
 
-# Create your models here.
+
+class Rates(models.Model):
+    base_rate = models.CharField(
+        'Базовая валюту',
+        max_length=10,
+        null=False,
+        blank=False,
+    )
+    current_rate = models.CharField(
+        'Курс валюты',
+        max_length=10,
+        null=False,
+        default=datetime.now,
+        db_index=True
+    )
+    timestamp = models.DateTimeField(
+        'Текущее время',
+        null=False,
+        default=datetime.now,
+    )
+
+    class Meta:
+        verbose_name = 'Курс валюты'
+        verbose_name_plural = 'Курсы валют'
+        ordering = ('timestamp',)
